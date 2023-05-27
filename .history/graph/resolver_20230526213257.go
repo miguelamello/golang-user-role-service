cf17fa3473c35c6ebbffd74d180bbc2204afa851
Miguel Angelo Mello] <miguelangelomello@gmail.com>
2023-05-26 21:42:59 -0300

    2023-05-26 21:42:59 by Miguel

---
+++ .history/graph/resolver_20230526213257.go
@@ -0,0 +1,13 @@
+package graph
+//go:generate go run github.com/99designs/gqlgen generate
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct{}
+
+func (r *Resolver) Query() graph.QueryResolver {
+	return &queryResolver{}
+}
+
